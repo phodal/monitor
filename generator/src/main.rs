84d83d5f362d69c4f242c247d26c3a1d9301716b
Phodal Huang <h@phodal.com>
2021-04-05 15:21:36 +0800

    feat: try to make text

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -1,41 +1,26 @@
-//! An example of generating julia fractals.
-extern crate image;
-extern crate num_complex;
+use image::{Rgb, RgbImage};
+use imageproc::drawing::{draw_text_mut, text_size};
+use rusttype::{Font, Scale};
+use std::path::Path;
 
 fn main() {
-    let imgx = 1280;
-    let imgy = 825;
+    let path = Path::new("monitor.bmp");
 
-    let scalex = 3.0 / imgx as f32;
-    let scaley = 3.0 / imgy as f32;
+    let mut image = RgbImage::new(1280, 825);
 
-    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+    let font = Vec::from(include_bytes!("DejaVuSans.ttf") as &[u8]);
+    let font = Font::try_from_vec(font).unwrap();
 
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let r = (0.3 * x as f32) as u8;
-        let b = (0.3 * y as f32) as u8;
-        *pixel = image::Rgb([r, 0, b]);
-    }
+    let height = 80.0;
+    let scale = Scale {
+        x: height * 2.0,
+        y: height,
+    };
 
-    for x in 0..imgx {
-        for y in 0..imgy {
-            let cx = y as f32 * scalex - 1.5;
-            let cy = x as f32 * scaley - 1.5;
+    let text = "Hello, world!";
+    draw_text_mut(&mut image, Rgb([255u8, 255u8, 255u8]), 0, 0, scale, &font, text);
+    let (w, h) = text_size(scale, &font, text);
+    println!("Text size: {}x{}", w, h);
 
-            let c = num_complex::Complex::new(-0.4, 0.6);
-            let mut z = num_complex::Complex::new(cx, cy);
-
-            let mut i = 0;
-            while i < 255 && z.norm() <= 2.0 {
-                z = z * z + c;
-                i += 1;
-            }
-
-            let pixel = imgbuf.get_pixel_mut(x, y);
-            let image::Rgb(data) = *pixel;
-            *pixel = image::Rgb([data[0], i as u8, data[2]]);
-        }
-    }
-
-    imgbuf.save("fractal.bmp").unwrap();
+    let _ = image.save(path).unwrap();
 }

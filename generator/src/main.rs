baebb01244c8def5125d7c03ee8820e288f2b7d0
Phodal Huang <h@phodal.com>
2021-04-05 22:11:14 +0800

    docs: try to make google calendar

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -7,48 +7,74 @@ use image::ImageBuffer;
 use serde::{Serialize, Deserialize};
 use reqwest::Client;
 
-#[derive(Deserialize, Serialize, Debug)]
-struct TaskList {
-    value: Vec<Item>,
-    #[serde(flatten, rename = "@odata.context")]
-    data_context: String,
-}
+extern crate hyper;
+extern crate hyper_rustls;
+extern crate yup_oauth2 as oauth2;
+extern crate google_calendar3 as calendar3;
 
+use calendar3::api::Channel;
+use std::default::Default;
+use calendar3::{CalendarHub, Error};
+use hyper::{Response, Body};
 
-#[derive(Deserialize, Serialize, Debug)]
-struct Item {
-    #[serde(flatten, rename = "@odata.etag")]
-    data_etag: String,
-    importance: String,
-    #[serde(flatten, rename = "isReminderOn")]
-    is_reminder_on: bool,
-    status: bool,
-    title: String,
-    #[serde(flatten, rename = "createdDateTime")]
-    created_date_time: String,
-    #[serde(flatten, rename = "lastModifiedDateTime")]
-    last_modified_date_time: String,
-    id: String,
-}
 
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
-    let request_url = format!("https://graph.microsoft.com/v1.0/me/todo/lists/{id}/tasks",
-                              id = "AQMkADAwATM0MDAAMS04N2E2LWRhMDItMDACLTAwCgAuAAADlaHHnFnn1UuoFE2pMt0j5QEAaMraZPkN_0mltv0IMNqe5wAD5QRmOQAAAA==");
-    println!("{}", request_url);
-    let client = Client::new();
-    let response =
-        client.get(&request_url)
-            .bearer_auth("")
-            .send()
-            .await?;
+async fn main() {
+    // Get an ApplicationSecret instance by some means. It contains the `client_id` and
+    // `client_secret`, among other things.
+    let mut secret: oauth2::ApplicationSecret = Default::default();
+    secret.client_id = "".to_string();
+    secret.client_secret = "".to_string();
+    secret.auth_uri = "https://accounts.google.com/o/oauth2/auth".to_string();
+    secret.project_id = Some("".to_string());
+    secret.auth_provider_x509_cert_url = Some("https://www.googleapis.com/oauth2/v1/certs".to_string());
+    secret.redirect_uris = vec!["http://localhost".to_string()];
 
-    println!("{:?}", response.text().await?);
-    // let users: Vec<TaskList> = response.json().await?;
-    // println!("{:?}", users);
-    write_text();
+    // Instantiate the authenticator. It will choose a suitable authentication flow for you,
+    // unless you replace  `None` with the desired Flow.
+    // Provide your own `AuthenticatorDelegate` to adjust the way it operates and get feedback about
+    // what's going on. You probably want to bring in your own `TokenStorage` to persist tokens and
+    // retrieve them from storage.
+    let auth = yup_oauth2::InstalledFlowAuthenticator::builder(
+        secret,
+        yup_oauth2::InstalledFlowReturnMethod::HTTPRedirect,
+    ).build().await.unwrap();
+    let mut hub = CalendarHub::new(hyper::Client::builder().build(hyper_rustls::HttpsConnector::with_native_roots()), auth);
+    // As the method needs a request, you would usually fill it with the desired information
+    // into the respective structure. Some of the parts shown here might not be applicable !
+    // Values shown here are possibly random and not representative !
+    let mut req = Channel::default();
 
-    Ok(())
+    // You can configure optional parameters by calling the respective setters at will, and
+    // execute the final call using `doit()`.
+    // Values shown here are possibly random and not representative !
+    let result = hub.calendar_list()
+        .list()
+        .doit()
+        .await;
+        // .doit().await;
+
+    match result {
+        Err(e) => match e {
+            // The Error enum provides details about what exactly happened.
+            // You can also just use its `Debug`, `Display` or `Error` traits
+            Error::HttpError(_)
+            | Error::Io(_)
+            | Error::MissingAPIKey
+            | Error::MissingToken(_)
+            | Error::Cancelled
+            | Error::UploadSizeLimitExceeded(_, _)
+            | Error::Failure(_)
+            | Error::BadRequest(_)
+            | Error::FieldClash(_)
+            | Error::JsonDecodeError(_, _) => {
+                println!("{}", e);
+            },
+        },
+        Ok(res) => {
+            println!("{:?}", res);
+        },
+    };
 }
 
 fn write_text() {

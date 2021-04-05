29dbe55ceeb3a1e21c3a52d6d5328032fe76727d
Phodal Huang <h@phodal.com>
2021-04-05 14:56:58 +0800

    feat(generator): use image as library

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -1,12 +1,41 @@
-#[macro_use]
-extern crate bmp;
-use bmp::{Image, Pixel};
+//! An example of generating julia fractals.
+extern crate image;
+extern crate num_complex;
 
 fn main() {
-    let mut img = Image::new(1280, 825);
+    let imgx = 1280;
+    let imgy = 825;
 
-    for (x, y) in img.coordinates() {
-        img.set_pixel(x, y, px!(x, y, 200));
+    let scalex = 3.0 / imgx as f32;
+    let scaley = 3.0 / imgy as f32;
+
+    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let r = (0.3 * x as f32) as u8;
+        let b = (0.3 * y as f32) as u8;
+        *pixel = image::Rgb([r, 0, b]);
     }
-    let _ = img.save("img.bmp");
+
+    for x in 0..imgx {
+        for y in 0..imgy {
+            let cx = y as f32 * scalex - 1.5;
+            let cy = x as f32 * scaley - 1.5;
+
+            let c = num_complex::Complex::new(-0.4, 0.6);
+            let mut z = num_complex::Complex::new(cx, cy);
+
+            let mut i = 0;
+            while i < 255 && z.norm() <= 2.0 {
+                z = z * z + c;
+                i += 1;
+            }
+
+            let pixel = imgbuf.get_pixel_mut(x, y);
+            let image::Rgb(data) = *pixel;
+            *pixel = image::Rgb([data[0], i as u8, data[2]]);
+        }
+    }
+
+    imgbuf.save("fractal.bmp").unwrap();
 }

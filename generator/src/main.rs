fd6876427b4aa4fba4b79ba3c7ecd814e2fa17af
Phodal Huang <h@phodal.com>
2021-04-07 10:21:08 +0800

    refactor: make readable

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -8,6 +8,7 @@ use reqwest::Client;
 use std::thread::sleep;
 use std::time::Duration;
 use chrono::prelude::*;
+use std::alloc::Global;
 
 #[derive(Deserialize, Serialize, Debug)]
 struct Quote {
@@ -28,10 +29,9 @@ async fn main() -> Result<(), reqwest::Error> {
                 .await?;
         let quote: Quote = response.json().await?;
 
-        let path = Path::new("monitor.bmp");
         let mut image = ImageBuffer::from_pixel(1280, 825, Rgb([255, 255, 255]));
-        write_sentence(quote.quote.as_str(), &mut image);
-        let _ = image.save(path).unwrap();
+        draw_image(quote.quote.as_str(), &mut image);
+        let _ = image.save(Path::new("monitor.bmp")).unwrap();
 
         execute_command();
     }
@@ -57,26 +57,35 @@ fn execute_command() {
     sleep(Duration::from_secs(60 * 30));
 }
 
-fn write_sentence(origin: &str, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>) {
+fn draw_image(origin: &str, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>) {
     let font = read_font();
 
-    let main_scale = Scale { x: 80.0, y: 80.0 };
-    let small_scale = Scale { x: 40.0, y: 40.0 };
 
+    let time_height = draw_time(image, &font);
+
+    draw_sentence(origin, image, &font, time_height)
+}
+
+fn draw_sentence(text: &str, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, font: &Font, offset: i32) {
     let sub_len = 31;
-    let subs = text_to_vec(origin, sub_len);
-
-    let time = time_now();
-    draw_text_mut(image, Rgb([0u8, 0u8, 0u8]), 0, 0, small_scale, &font, time.as_str());
-
-    let offset = 40;
-
+    let subs = text_to_vec(text, sub_len);
     let mut index = 0;
+    let main_scale = Scale { x: 80.0, y: 80.0 };
     for sub in subs {
         let y = index * 80 + offset;
         draw_text_mut(image, Rgb([0u8, 0u8, 0u8]), 0, y, main_scale, &font, sub.as_str());
         index = index + 1;
     }
+}
+
+fn draw_time(image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, font: &Font) -> i32 {
+    let small_scale = Scale { x: 40.0, y: 40.0 };
+
+    let time = time_now();
+    draw_text_mut(image, Rgb([0u8, 0u8, 0u8]), 0, 0, small_scale, &font, time.as_str());
+
+    let offset = 40;
+    offset
 }
 
 fn read_font() -> Font<'static> {

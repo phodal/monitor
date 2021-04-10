f8772459ce092e3c2537b2d9316cd17829b482a6
Phodal Huang <h@phodal.com>
2021-04-10 14:13:57 +0800

    build: add todo model

---
+++ generator/src/todo.rs
@@ -0,0 +1,31 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Todo {
+    #[serde(rename = "@odata.context")]
+    pub odata_context: String,
+    pub value: Vec<TodoItem>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TodoItem {
+    #[serde(rename = "@odata.etag")]
+    pub odata_etag: String,
+    pub importance: String,
+    pub is_reminder_on: bool,
+    pub status: String,
+    pub title: String,
+    pub created_date_time: String,
+    pub last_modified_date_time: String,
+    pub id: String,
+    pub body: TodoBody,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TodoBody {
+    pub content: String,
+    pub content_type: String,
+}
